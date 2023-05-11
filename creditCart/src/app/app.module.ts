ce4eb2b345444244c689745d7a2c9359cae5d97a
Acelya777 <acelyayildirim2020@gmail.com>
2023-05-11 11:46:17 +0300

    Initial commit

---
+++ creditCart/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardformComponent } from './cardform/cardform.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { InputComponent } from './input/input.component'
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardformComponent,
+    InputComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ creditCart/src/app/input/input.component.ts
@@ -0,0 +1,17 @@
+import { Component,Input,OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent {
+ @Input() control!:FormControl;
+ @Input() label!:string;
+
+ showErrors(){
+  // const {dirty,touched,errors}=this.control;
+  return this.control.dirty&&this.control.touched&&this.control.errors;
+ }
+ 
+}
